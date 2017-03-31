1fdd2c962f24e0da2a20767995649b219887730a
scaperoth <scaperoth@gmail.com>
2017-03-31 12:43:16 -0400

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+	private PlayerController playerController;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		playerController = player.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (!playerController.gameOver) {
+			transform.position = player.transform.position + offset;
+		} 
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public Text countText;
+	public Text countTime;
+	public Text gameOverText;
+	public bool gameOver;
+
+	private Rigidbody rb;
+	private int count;
+	private int numPickUps;
+	private float time;
+	private string pickUpTag;
+
+	void Start ()
+	{
+		pickUpTag = "Pick Up";
+		gameOverText.text = "";
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		time = 0.00f;
+		numPickUps = GetNumPickUps ();
+		gameOver = false;
+
+		SetCountText ();	
+		SetTimeText ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		float vectorMovementMultiplier = 15f;
+
+		Vector3 movement = new Vector3 (moveHorizontal * vectorMovementMultiplier, 0.0f, moveVertical * vectorMovementMultiplier);
+		SetTimeText ();
+		rb.AddForce (movement);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag (pickUpTag)) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+
+			numPickUps = GetNumPickUps ();
+			if (numPickUps == 0) {
+				gameOver = true;
+				gameOverText.text = "GAME OVER";
+			}
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+	}
+
+	void SetTimeText ()
+	{
+		time += Time.deltaTime;
+		countTime.text = "Time: " + time.ToString ("F2");
+	}
+
+	int GetNumPickUps ()
+	{
+		return GameObject.FindGameObjectsWithTag (pickUpTag).Length;
+	}
+
+}
